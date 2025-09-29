7e75014d6b59b3af7e1a086a685532c0082574a3
Bryce S <brycebox@gmail.com>
2025-09-29 15:59:13 -0600

    feat: random camera positions

--- src/ThreeDModel.tsx
+++ src/ThreeDModel.tsx
@@ -1,7 +1,7 @@
-import { Suspense, useState } from "react"
+import { Suspense, useRef, useState } from "react"
 import { Canvas } from "@react-three/fiber"
-import { Loader, OrbitControls } from "@react-three/drei"
-import { EffectComposer, Bloom } from "@react-three/postprocessing"
+import { CameraControls, Loader } from "@react-three/drei"
+import { Bloom, EffectComposer } from "@react-three/postprocessing"
 import { AirplaneModel } from "./Airplane"
 import type { Vector3D } from "./types"
 
@@ -9,26 +9,33 @@ const cameraPresets: Record<string, Vector3D> = {
   oncomingLR: [-70, 10, -50],
   oncoming: [-70, 10, 0],
   oncomingRL: [-70, 10, 50],
+  outgoingRL: [70, 10, 50],
+  outgoingLR: [70, 10, 50],
+  outgoing: [70, 10, 50],
 }
-// const getRandomCameraPreset = () => {
-//   const keys = Object.keys(cameraPresets)
-//   const randomIndex = Math.floor(Math.random() * keys.length)
-//   return cameraPresets[keys[randomIndex]]
-// }
+const getRandomCameraPreset = () => {
+  const keys = Object.keys(cameraPresets)
+  const randomIndex = Math.floor(Math.random() * keys.length)
+  return cameraPresets[keys[randomIndex]]
+}
 
 export function ThreeDModel() {
   const [lightsOn, setLightsOn] = useState(false)
-  const [cameraPosition] = useState<Vector3D>(cameraPresets.oncoming)
+  const cameraControlsRef = useRef<CameraControls>(null)
+
+  const moveCamera = () => {
+    const newPos = getRandomCameraPreset()
+    const [x, y, z] = newPos.map((coord) => coord * -1)
+    cameraControlsRef.current?.lookInDirectionOf(x, y, z, false)
+  }
 
   return (
     <div className="ThreeDModel">
       <button onClick={() => setLightsOn(!lightsOn)}>
         Night Mode {lightsOn ? "on" : "off"}
       </button>
-      <button disabled title="work in progress">
-        Randomize Orientation
-      </button>
-      <Canvas camera={{ position: cameraPosition, fov: 50 }}>
+      <button onClick={moveCamera}>Randomize Orientation</button>
+      <Canvas camera={{ position: cameraPresets.oncoming, fov: 50 }}>
         <ambientLight intensity={lightsOn ? 0.2 : 0.01} />
         <directionalLight
           position={[5, 10, 7]}
@@ -37,12 +44,13 @@ export function ThreeDModel() {
         <Suspense fallback={null}>
           <AirplaneModel />
         </Suspense>
-        <OrbitControls enablePan={false} enableZoom />
+        <CameraControls ref={cameraControlsRef} />
         <EffectComposer>
           <Bloom
-            luminanceThreshold={1}
-            luminanceSmoothing={0.9}
-            intensity={2}
+            luminanceThreshold={0.03}
+            luminanceSmoothing={0.1}
+            intensity={0.5}
+            mipmapblur
           />
         </EffectComposer>
       </Canvas>
