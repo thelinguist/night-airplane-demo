bb97d50ac642a35f2a1536b8e0621b29b3909d5b
Bryce S <brycebox@gmail.com>
2025-09-29 16:57:00 -0600

    feat: standardized controls

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,14 +3,14 @@ import { cameraPresets } from "../constants"
 import { CameraControls } from "@react-three/drei"
 import type { RefObject } from "react"
 
-export const getRandomKey = <T = object>(object: T) => {
+export const getRandomValue = <T = object>(object: T) => {
   const keys = Object.keys(object as object)
   const randomIndex = Math.floor(Math.random() * keys.length)
   return object[keys[randomIndex] as keyof T]
 }
 
 export const moveCamera = (cameraControlsRef: RefObject<CameraControls | null>, newPos?: Vector3D) => {
-  const position = !newPos ? getRandomKey(cameraPresets) : newPos
+  const position = newPos ?? getRandomValue(cameraPresets)
   const [x, y, z] = position.map((coord) => coord * -1)
   cameraControlsRef.current?.lookInDirectionOf(x, y, z, false)
 }
